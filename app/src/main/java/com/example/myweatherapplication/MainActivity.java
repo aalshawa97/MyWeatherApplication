3154e2c355f97c084f5a3fdd55132c45c7a4d3e0
aalshawa97 <abdullahalshawa@gmail.com>
2022-03-01 14:35:13 -0800

    Weather application

--- app/src/main/java/com/example/myweatherapplication/MainActivity.java
+++ app/src/main/java/com/example/myweatherapplication/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.myweatherapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //private RelativeLayout homeRL;
+        //p
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
