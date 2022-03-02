a72b887b3e4046c0e84211b419e8cd6cc8e621b2
aalshawa97 <abdullahalshawa@gmail.com>
2022-03-02 09:58:52 -0800

    Updated UI

--- app/src/main/java/com/example/myweatherapplication/MainActivity.java
+++ app/src/main/java/com/example/myweatherapplication/MainActivity.java
@@ -1,17 +1,37 @@
 package com.example.myweatherapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //private RelativeLayout homeRL;
-        //p
+        RelativeLayout homeRL;
+        ProgressBar loadingPB;
+        TextView cityNameTV, temperatureTV, conditionTV;
+        RecyclerView weatherRV;
+        TextInputEditText cityEdt;
+        ImageView backIV, iconIV, searchIV;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        homeRL = findViewById(R.id.idRLHome);
+        loadingPB = findViewById(R.id.idPBLoading);
+        cityNameTV = findViewById(R.id.idTVCityName);
+        temperatureTV = findViewById(R.id.idTvVTemperature);
+        conditionTV = findViewById(R.id.idTVCondition);
+        weatherRV = findViewById(R.id.idRvWeather);
+        cityEdt = findViewById(R.id.idEdtCity);
+        backIV = findViewById(R.id.idIVBack);
+        iconIV = findViewById(R.id.idTVIcon);
+        searchIV = findViewById(R.id.idTVSearch);
     }
 }
