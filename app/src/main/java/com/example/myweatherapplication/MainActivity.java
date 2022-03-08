d1e7b203ec5208bd5c09435a6aad3d405e2108ec
aalshawa97 <abdullahalshawa@gmail.com>
2022-03-08 08:08:46 -0800

    Initial Commit

--- app/src/main/java/com/example/myweatherapplication/MainActivity.java
+++ app/src/main/java/com/example/myweatherapplication/MainActivity.java
@@ -1,28 +1,50 @@
 package com.example.myweatherapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
+public class MainActivity extends AppCompatActivity {
+    private RelativeLayout homeRL;
+    private ProgressBar loadingPB;
+    private TextView cityNameTV, temperatureTV, conditionTV;
+    private RecyclerView weatherRV;
+    private TextInputEditText cityEdt;
+    private ImageView backIV, iconIV, searchIV;
+    private ArrayList<WeatherRVModel> weatherRVModelArrayList;
+    private WeatherRVAdapter weatherRVAdapter;
+    private LocationManager locationManager;
+    private int PERMISSION_CODE = 1;
+    private String cityName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*
-        RelativeLayout homeRL;
-        ProgressBar loadingPB;
-        TextView cityNameTV, temperatureTV, conditionTV;
-        RecyclerView weatherRV;
-        TextInputEditText cityEdt;
-        ImageView backIV, iconIV, searchIV;
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_main);
         homeRL = findViewById(R.id.idRLHome);
         loadingPB = findViewById(R.id.idPBLoading);
@@ -30,10 +52,67 @@ public class MainActivity extends AppCompatActivity {
         temperatureTV = findViewById(R.id.idTvVTemperature);
         conditionTV = findViewById(R.id.idTVCondition);
         weatherRV = findViewById(R.id.idRvWeather);
-        cityEdt = findViewById(R.id.idEdtCity);
+        cityEdt = (TextInputEditText)findViewById(R.id.idEdtCity);
         backIV = findViewById(R.id.idIVBack);
         iconIV = findViewById(R.id.idTVIcon);
         searchIV = findViewById(R.id.idTVSearch);
-        */
+        weatherRVModelArrayList = new ArrayList<>();
+        weatherRVAdapter = new WeatherRVAdapter(this, weatherRVModelArrayList);
+        weatherRV.setAdapter(weatherRV.getAdapter());
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_CODE);
+        }
+        //ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_CODE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        cityName = getCityName(location.getLongitude(), location.getLatitude());
+        getWeatherInfo(cityName);
+        searchIV.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String city = cityEdt.getText().toString();
+                if(city.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Please enter city name", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    cityNameTV.setText(cityName);
+                    getWeatherInfo(city);
+                }
+            }
+
+            String city = cityEdt.getText().toString();
+        });
+    }
+    private String getCityName(double longitude, double latitude){
+        String cityName = "Not found";
+        Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+        try{
+            List<Address> addresses = gcd.getFromLocation(latitude, longitude, 10);
+            for(Address adr : addresses){
+                if(adr != null){
+                    String city = adr.getLocality();
+                    if(city != null && city.equals("")){
+                        cityName = city;
+                    }
+                    else{
+                        Log.d("TAG", "City Not Found");
+                        Toast.makeText(this,"User City Not Found....", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return cityName;
+    }
+
+    public void onRequestPermissonsResult()
+    {
+
+    }
+    private void getWeatherInfo(String cityName){
+        String url = "http://api.weatherapi.com/v1/current.json?key=415f2bfdb47b49feae9232440220103&q=" + cityName +"Gaza&aqi=yes\n";
     }
 }
